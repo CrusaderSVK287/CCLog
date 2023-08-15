cbf4e13f9cf83af5d6285932a0155b68ec8e6814
Lukáš Belán <lukasbelan27@gmail.com>
2023-08-15 17:31:25 +0200

    Added utility functions to convert boolean to string and vice versa

--- src/utils/utils.c
+++ src/utils/utils.c
@@ -1,4 +1,6 @@
 #include "utils.h"
+#include <stdbool.h>
+#include <string.h>
 
 struct tm *util_current_timeinfo()
 {
@@ -10,5 +12,15 @@ struct tm *util_current_timeinfo()
 
         /* Return the struct */
         return timeinfo;
+}
+
+const char *bool_to_str(bool b)
+{
+        return (b) ? "true" : "false";
+}
+
+bool str_to_bool(const char *s)
+{
+        return (!strcmp(s, "true")) ? true : false;
 }
 
