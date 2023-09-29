22c8a8283452d18e1bbbfe931b48b52fcecdaa82
Lukáš Belán <lukasbelan27@gmail.com>
2023-09-30 01:47:22 +0200

    Change to multy file naming scheme to avoid escape characters

--- src/logger_init.c
+++ src/logger_init.c
@@ -47,7 +47,7 @@ static int open_multiple_file_mode(const char *path, const char *proc_name)
         /* Get current time to prepend to filename */
         struct tm *timeinfo = util_current_timeinfo();
 
-        sprintf(time_buf ,"[%04d-%02d-%02d_%02d:%02d:%02d]", 
+        sprintf(time_buf ,"%04d-%02d-%02d_%02d:%02d:%02d", 
                 timeinfo->tm_year + 1900, timeinfo->tm_mon + 1, 
                 timeinfo->tm_mday, 
                 timeinfo->tm_hour, timeinfo->tm_min, timeinfo->tm_sec);
